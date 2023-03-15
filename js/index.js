06f14acf42b9e21dfb5cf48e5025dcb1e9413421
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2023-03-15 12:26:32 +0100

    Message render

--- js/index.js
+++ js/index.js
@@ -4,19 +4,67 @@ const chat = {
     author: "Pedro",
     init() {
         this.fetchMessages();
+
     },
     sendMessage() {
+        fetch('https://dev2chat.onrender.com/message',
+        {
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify(data)
+
+        })
+        document.querySelector('#chatForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            console.log('berichtje');
+        })
+        this.fetchMessages
     },
     fetchMessages() {
         fetch('https://dev2chat.onrender.com/messages')
         .then(incommingMessage => incommingMessage.json())
-        .then(data => console.log(data))
+        .then(function (data) {
+            console.log(data);
+           
+            data.sort(function(a, b){
+                if (a.id < b.id){
+                    return -1
+                } else {
+                    return 1
+                }
+            })
+           data.forEach(function(data) {
+            const htmlMessage = `
+            <div class="messageItem">
+                <div class="header">
+                    <span class="author">${data.author}</span>
+                    <span class="time">${data.created_at}</span>
+                </div>
+                <p class="messageLine">
+                 ${data.message}
+                </p>
+            </div>
+      `
+
+          document.querySelector('#messageContainer').insertAdjacentHTML('beforeend', htmlMessage)
+            
+           });
+            
+            
+        });
         
-        // document.querySelector('.author').innerHTML(.author)
+        
     },
     renderMessage(message) {
     }
 
+}
+
+function karsten() {
+    const lover = "dreamPartner";
 }
 
 chat.init();
