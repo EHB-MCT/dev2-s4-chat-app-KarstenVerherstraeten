f3d82ae39fbc6566c4cd19456d1959dcbb63b6f9
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2023-03-15 10:24:45 +0100

    fetchMessage

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 "use strict";
 
 const chat = {
-    author: "yourName",
+    author: "Pedro",
     init() {
+        this.fetchMessages();
     },
     sendMessage() {
     },
     fetchMessages() {
+        fetch('https://dev2chat.onrender.com/messages')
+        .then(incommingMessage => incommingMessage.json())
+        .then(data => console.log(data))
+        
+        // document.querySelector('.author').innerHTML(.author)
     },
     renderMessage(message) {
     }
